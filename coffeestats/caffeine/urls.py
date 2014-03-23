9773a8c93711a41e997f0dda41d93afc328e3dac
Jan Dittberner <jan@dittberner.info>
2014-03-23 17:13:46 +0100

    define all URLs required on index page
    - caffeine.urls:
      - define named URLs for pages referenced on the index page
    - caffeine.views:
      - define dummy template views for all pages referenced on the index
        page

--- coffeestats/caffeine/urls.py
+++ coffeestats/caffeine/urls.py
@@ -3,10 +3,22 @@
 from django.conf.urls import patterns, url
 
 from .views import (
+    AboutView,
+    ExploreView,
+    ImprintView,
     IndexView,
+    OverallView,
+    ProfileView,
+    SettingsView,
 )
 
 urlpatterns = patterns(
     '',
     url(r'^$', IndexView.as_view(), name='home'),
+    url(r'^about/$', AboutView.as_view(), name='about'),
+    url(r'^explore/$', ExploreView.as_view(), name='explore'),
+    url(r'^imprint/$', ImprintView.as_view(), name='imprint'),
+    url(r'^overall/$', OverallView.as_view(), name='overall'),
+    url(r'^settings/$', SettingsView.as_view(), name='settings'),
+    url(r'^profile/$', ProfileView.as_view(), name='profile'),
 )
--- coffeestats/caffeine/views.py
+++ coffeestats/caffeine/views.py
@@ -2,5 +2,29 @@ from django.views.generic import TemplateView
 from braces.views import LoginRequiredMixin
 
 
+class AboutView(LoginRequiredMixin, TemplateView):
+    template_name = 'about.html'
+
+
+class ExploreView(LoginRequiredMixin, TemplateView):
+    template_name = 'explore.html'
+
+
+class ImprintView(TemplateView):
+    template_name = 'imprint.html'
+
+
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
+
+
+class OverallView(LoginRequiredMixin, TemplateView):
+    template_name = 'overall.html'
+
+
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
+
+
+class SettingsView(LoginRequiredMixin, TemplateView):
+    template_name = 'settings.html'

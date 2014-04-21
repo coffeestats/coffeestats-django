458b8b377a83b92e4299f69ee8d16d9f604749a1
Jan Dittberner <jan@dittberner.info>
2014-04-21 13:18:10 +0200

    refactor caffeine test modules
    - move tests into module directory
    - split authentication tests into separate methods for each concern
    - add placeholder files for model, form and view tests

---
+++ coffeestats/caffeine/tests/test_authentication.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+
+from django.contrib.auth.hashers import check_password
+
+from passlib.hash import bcrypt
+
+from caffeine.models import User
+from caffeine.authbackend import LegacyCoffeestatsAuth
+
+
+class LegacyCoffeestatsAuthTest(TestCase):
+    def setUp(self):
+        self.user, created = User.objects.get_or_create(
+            username='testuser')
+        self.user.cryptsum = bcrypt.encrypt('test', ident='2y')
+        self.user.password = ''
+        self.user.save()
+
+        self.legacyauth = LegacyCoffeestatsAuth()
+
+    def test_authenticate_nonexistant_user(self):
+        self.assertIsNone(
+            self.legacyauth.authenticate('nouser', 'doesntmatter'))
+
+    def test_authenticate_user_with_wrong_password(self):
+        self.assertIsNone(self.legacyauth.authenticate('testuser', 'wrong'))
+
+    def test_authenticate_valid_user(self):
+        loggedin = self.legacyauth.authenticate('testuser', 'test')
+
+        self.assertIsNotNone(loggedin)
+        self.assertEqual(loggedin.cryptsum, '')
+        self.assertTrue(check_password('test', loggedin.password))
+
+        self.assertEqual(self.legacyauth.get_user(loggedin.id), loggedin)
+
+    def test_authenticate_only_works_once(self):
+        """
+        Check that subsequent authentication fails, because user has been
+        migrated.
+        """
+        self.assertIsNotNone(
+            self.legacyauth.authenticate('testuser', 'test'))
+        self.assertIsNone(
+            self.legacyauth.authenticate('testuser', 'test'))
+
+    def test_get_nonexistent_user(self):
+        self.assertIsNone(self.legacyauth.get_user(-10))
+
+    def test_get_existant_user(self):
+        self.assertEqual(self.legacyauth.get_user(self.user.id), self.user)

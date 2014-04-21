98d84c97ce22cd4f9bfda0892f201a0927784ede
Jan Dittberner <jan@dittberner.info>
2014-04-21 12:36:27 +0200

    add functional tests for homepage and imprint

---
+++ coffeestats/functional_tests/base.py
@@ -0,0 +1,49 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+
+class SeleniumTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = webdriver.Firefox()
+        cls.selenium.implicitly_wait(3)
+        super(SeleniumTest, cls).setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+
+
+class BaseCoffeeStatsPageTestMixin(object):
+
+    def check_page_header(self):
+        # The caffeine junkie sees that the page title mention coffeestats and
+        # looks at the awesome headline and slogan in the header
+        self.assertIn('coffeestats', self.selenium.title)
+        header = self.selenium.find_element_by_id('header')
+        # the title links to the home page
+        homelink = header.find_element_by_link_text('coffeestats.org')
+        self.assertEqual(homelink.get_attribute('href'),
+                         '{}/'.format(self.server_url))
+
+        # the header contains the awesome slogan
+        self.assertIn('... about what keeps you awake at night', header.text)
+
+    def check_page_footer(self):
+        # the footer contains links to the home page, the authors' web sites
+        # and the imprint
+        footer = self.selenium.find_element_by_class_name('footer')
+
+        expected_footer_links = [
+            (u'coffeestats.org', u'{}/'.format(self.server_url)),
+            (u'Jan Dittberner', u'http://jan.dittberner.info/'),
+            (u'Florian Baumann', u'http://noqqe.de/'),
+            (u'Imprint', u'{}/imprint/'.format(self.server_url))
+        ]
+
+        self.assertEqual(
+            [(link.text, link.get_attribute('href')) for link in
+             footer.find_elements_by_tag_name('a')],
+            expected_footer_links)
---
+++ coffeestats/functional_tests/test_base_pages.py
@@ -0,0 +1,78 @@
+from .base import BaseCoffeeStatsPageTestMixin, SeleniumTest
+
+
+class BasicPageTest(BaseCoffeeStatsPageTestMixin, SeleniumTest):
+
+    def test_check_homepage_elements(self):
+        # Coffeejunkie has heard about coffeestats and wants to visit the
+        # homepage
+        self.selenium.get(self.server_url)
+
+        self.check_page_header()
+
+        # He finds out that he was redirected to a login page
+        self.assertRegexpMatches(self.selenium.current_url,
+                                 r'/auth/login/\?next=/$')
+
+        # He sees 3 boxes
+        boxes = self.selenium.find_elements_by_css_selector('.white-box')
+        self.assertEqual(len(boxes), 3)
+
+        # The first box contains login form elements
+        login_box = boxes[0]
+        login_title = login_box.find_element_by_tag_name('h2')
+        self.assertEqual(login_title.text, 'Login')
+
+        form_inputs = login_box.find_elements_by_tag_name('input')
+        self.assertEqual(len(form_inputs), 5)
+
+        # the input fields contain placeholders
+        self.assertEqual(login_box.find_element_by_id(
+            'id_username').get_attribute('placeholder'), 'Username')
+        self.assertEqual(login_box.find_element_by_id(
+            'id_password').get_attribute('placeholder'), 'Password')
+
+        # there is a "Login" button
+        login_button = login_box.find_element_by_name('submit')
+        self.assertEqual(login_button.get_attribute('value'), 'Login')
+
+        # there is a "Register" link
+        register_link = login_box.find_element_by_link_text('Register')
+        self.assertRegexpMatches(
+            register_link.get_attribute('href'),
+            r'/auth/register/$')
+
+        # there is a "Password reset" link
+        pwreset_link = login_box.find_element_by_link_text(
+            'Request a password reset')
+        self.assertRegexpMatches(
+            pwreset_link.get_attribute('href'),
+            r'/auth/password/reset/$')
+
+        # The second box has some information about coffeestats
+        self.assertEqual(boxes[1].find_element_by_tag_name('h2').text,
+                         'What is coffeestats.org?')
+
+        # The third box tells something about graphs
+        graph_box = boxes[2]
+        self.assertEqual(graph_box.find_element_by_tag_name('h2').text,
+                         'Graphs!')
+        examplegraph = graph_box.find_element_by_id('coffeeexample')
+        self.assertEqual(examplegraph.tag_name, 'canvas')
+
+        self.check_page_footer()
+
+    def test_imprint_elements(self):
+        # The caffeine junkie visits coffeestats.org
+        self.selenium.get(self.server_url)
+
+        # He wants to see who made this site and clicks the "Imprint" link
+        self.selenium.find_element_by_link_text('Imprint').click()
+
+        self.check_page_header()
+
+        self.assertEqual(len(
+            self.selenium.find_elements_by_class_name('white-box')), 1
+        )
+
+        self.check_page_footer()

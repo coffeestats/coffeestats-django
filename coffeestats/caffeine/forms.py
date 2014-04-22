2b94ac1cbb080da318545e7e1051c2fa54588831
Jan Dittberner <jan@dittberner.info>
2014-04-22 21:47:19 +0200

    add form tests
    - refactor caffeine.forms:
      - extract error messages into module level constants
      - show password mismatch error if only one password field is filled in
        SettingsForm
      - use Django mechanisms for required field handling in SelectTimeZoneForm
      - set instance fields in SubmitCaffeineForm.__init__ instead of overriding
        save method
      - override SubmitCaffeineForm.clean_date instead of SubmitCaffeineForm.clean
    - implement tests for all caffeine.forms

--- coffeestats/caffeine/forms.py
+++ coffeestats/caffeine/forms.py
@@ -14,6 +14,15 @@ from .models import (
     User,
 )
 
+DUPLICATE_USER_ERROR = _("A user with that username already exists.")
+DUPLICATE_EMAIL_ERROR = _(
+    "This email address is already in use. "
+    "Please supply a different email address."
+)
+PASSWORD_MISMATCH_ERROR = _('Passwords must match!')
+INVALID_TIMEZONE_ERROR = _("Invalid time zone name")
+EMPTY_TIMEZONE_ERROR = _("Time zone must not be empty.")
+
 
 class CoffeestatsRegistrationForm(RegistrationFormUniqueEmail):
     firstname = forms.CharField(label=_("First name"), required=False)
@@ -30,8 +39,7 @@ class CoffeestatsRegistrationForm(RegistrationFormUniqueEmail):
         existing = User.objects.filter(
             username__iexact=self.cleaned_data['username'])
         if existing.exists():
-            raise forms.ValidationError(
-                _("A user with that username already exists."))
+            raise forms.ValidationError(DUPLICATE_USER_ERROR)
         else:
             return self.cleaned_data['username']
 
@@ -42,9 +50,7 @@ class CoffeestatsRegistrationForm(RegistrationFormUniqueEmail):
 
         """
         if User.objects.filter(email__iexact=self.cleaned_data['email']):
-            raise forms.ValidationError(
-                _("This email address is already in use. "
-                  "Please supply a different email address."))
+            raise forms.ValidationError(DUPLICATE_EMAIL_ERROR)
         return self.cleaned_data['email']
 
 
@@ -62,8 +68,8 @@ class SettingsForm(forms.ModelForm):
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
 
-        if password2 and password1 and password1 != password2:
-            raise forms.ValidationError(_('Passwords must match!'))
+        if (password2 or password1) and password1 != password2:
+            raise forms.ValidationError(PASSWORD_MISMATCH_ERROR)
         return password2
 
     def clean_email(self):
@@ -75,9 +81,7 @@ class SettingsForm(forms.ModelForm):
         emailuser = User.objects.filter(
             email__iexact=self.cleaned_data['email']).first()
         if emailuser is not None and emailuser != self.instance:
-            raise forms.ValidationError(
-                _("This email address is already in use. "
-                  "Please supply a different email address."))
+            raise forms.ValidationError(DUPLICATE_EMAIL_ERROR)
         if self.cleaned_data['email'] != self.instance.email:
             self.email_action = Action.objects.create_action(
                 self.instance, ACTION_TYPES.change_email,
@@ -88,7 +92,8 @@ class SettingsForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super(SettingsForm, self).clean()
-        if self.cleaned_data['password2']:
+        if 'password2' in self.cleaned_data and \
+                self.cleaned_data['password2']:
             self.password_set = True
             self.instance.set_password(
                 self.cleaned_data['password2'])
@@ -99,18 +104,23 @@ class SelectTimeZoneForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['timezone']
+        error_messages = {
+            'timezone': {
+                'required': EMPTY_TIMEZONE_ERROR,
+            },
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(SelectTimeZoneForm, self).__init__(*args, **kwargs)
+        self.fields['timezone'].required = True
 
     def clean_timezone(self):
         timezone = self.cleaned_data['timezone']
-        if timezone:
-            try:
-                with override_timezone(timezone):
-                    return timezone
-            except:
-                raise forms.ValidationError(
-                    _("Invalid time zone name"))
-        raise forms.ValidationError(
-            _("Time zone must not be empty."))
+        try:
+            with override_timezone(timezone):
+                return timezone
+        except:
+            raise forms.ValidationError(INVALID_TIMEZONE_ERROR)
 
 
 class SubmitCaffeineForm(forms.ModelForm):
@@ -119,31 +129,25 @@ class SubmitCaffeineForm(forms.ModelForm):
         fields = ['date']
 
     def __init__(self, user, ctype, *args, **kwargs):
-        self.user = user
-        self.ctype = getattr(DRINK_TYPES, ctype)
         super(SubmitCaffeineForm, self).__init__(*args, **kwargs)
+        self.instance.ctype = ctype
+        self.instance.user = user
+        self.instance.timezone = user.timezone
 
-    def save(self):
-        caffeine = Caffeine(user=self.user, ctype=self.ctype,
-                            date=self.cleaned_data['date'],
-                            timezone=self.user.timezone)
-        caffeine.save()
-        return caffeine
-
-    def clean(self):
-        cleaned_data = super(SubmitCaffeineForm, self).clean()
+    def clean_date(self):
         recent_caffeine = Caffeine.objects.find_recent_caffeine(
-            self.user, cleaned_data['date'], self.ctype)
+            self.instance.user, self.cleaned_data['date'],
+            self.instance.ctype)
         if recent_caffeine:
             raise forms.ValidationError(
                 _('Your last %(drink)s was less than %(minutes)d minutes '
                   'ago at %(date)s %(timezone)s'),
                 code='drinkfrequency',
                 params={
-                    'drink': DRINK_TYPES[self.ctype],
+                    'drink': DRINK_TYPES[self.instance.ctype],
                     'minutes': settings.MINIMUM_DRINK_DISTANCE,
                     'date': recent_caffeine.date,
-                    'timezone': self.user.timezone
+                    'timezone': recent_caffeine.timezone
                 }
             )
-        return cleaned_data
+        return self.cleaned_data['date']
---
+++ coffeestats/caffeine/tests/test_forms.py
@@ -0,0 +1,224 @@
+from datetime import timedelta
+import os
+
+from django.conf import settings
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+from caffeine.forms import (
+    CoffeestatsRegistrationForm,
+    DUPLICATE_EMAIL_ERROR,
+    DUPLICATE_USER_ERROR,
+    EMPTY_TIMEZONE_ERROR,
+    INVALID_TIMEZONE_ERROR,
+    PASSWORD_MISMATCH_ERROR,
+    SelectTimeZoneForm,
+    SettingsForm,
+    SubmitCaffeineForm,
+)
+from caffeine.models import (
+    Action,
+    ACTION_TYPES,
+    Caffeine,
+    DRINK_TYPES,
+)
+
+
+User = get_user_model()
+os.environ['RECAPTCHA_TESTING'] = 'True'
+
+
+class CoffeestatsRegistrationFormTest(TestCase):
+
+    def test_clean_username_new_user(self):
+        form = CoffeestatsRegistrationForm(
+            data={'username': 'testuser',
+                  'email': 'test@bla.com',
+                  'password1': 'test1234',
+                  'password2': 'test1234',
+                  'recaptcha_response_field': 'PASSED'}
+        )
+        self.assertTrue(form.is_valid(), str(form.errors))
+        self.assertEqual(form.clean_username(), 'testuser')
+
+    def test_clean_username_existing_user(self):
+        User.objects.create_user('testuser', 'test@bla.com')
+        form = CoffeestatsRegistrationForm(
+            data={'username': 'testuser',
+                  'email': 'test@example.org',
+                  'password1': 'test1234',
+                  'password2': 'test1234',
+                  'recaptcha_response_field': 'PASSED'}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [DUPLICATE_USER_ERROR])
+
+    def test_clean_email_new_user(self):
+        form = CoffeestatsRegistrationForm(
+            data={'username': 'testuser',
+                  'email': 'test@bla.com',
+                  'password1': 'test1234',
+                  'password2': 'test1234',
+                  'recaptcha_response_field': 'PASSED'}
+        )
+        self.assertTrue(form.is_valid(), str(form.errors))
+        self.assertEqual(form.clean_email(), 'test@bla.com')
+
+    def test_clean_email_existing_email(self):
+        User.objects.create_user('testuser', 'test@bla.com')
+        form = CoffeestatsRegistrationForm(
+            data={'username': 'testuser2',
+                  'email': 'test@bla.com',
+                  'password1': 'test1234',
+                  'password2': 'test1234',
+                  'recaptcha_response_field': 'PASSED'}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], [DUPLICATE_EMAIL_ERROR])
+
+
+class SettingsFormTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='test@bla.com',
+            password='test1234')
+
+    def test_clean_password2_empty(self):
+        form = SettingsForm(data={}, instance=self.user)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(self.user.check_password('test1234'))
+
+    def test_clean_password2_valid(self):
+        self.assertTrue(self.user.check_password('test1234'))
+        form = SettingsForm(data={
+            'password1': 'test2345',
+            'password2': 'test2345'
+        }, instance=self.user)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(self.user.check_password('test2345'))
+
+    def test_clean_password2_invalid(self):
+        self.assertTrue(self.user.check_password('test1234'))
+        form = SettingsForm(data={
+            'password1': 'test2345',
+            'password2': 'wrongone'
+        }, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password2'],
+                         [PASSWORD_MISMATCH_ERROR])
+        self.assertTrue(self.user.check_password('test1234'))
+
+    def test_clean_password2_one_empty(self):
+        form = SettingsForm(data={
+            'password1': 'test2345',
+            'password2': ''
+        }, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password2'],
+                         [PASSWORD_MISMATCH_ERROR])
+        self.assertTrue(self.user.check_password('test1234'))
+
+    def test_clean_email_no_change(self):
+        form = SettingsForm(data={
+            'email': 'test@bla.com'}, instance=self.user)
+        self.assertTrue(form.is_valid())
+
+    def test_clean_email_change_user(self):
+        form = SettingsForm(data={
+            'email': 'test@blub.com'}, instance=self.user)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(len(Action.objects.all()), 1)
+        self.assertEqual(Action.objects.all()[0].data, 'test@blub.com')
+        self.assertEqual(Action.objects.all()[0].user, self.user)
+        self.assertEqual(Action.objects.all()[0].atype,
+                         ACTION_TYPES.change_email)
+
+    def test_clean_email_duplicate_address(self):
+        User.objects.create_user(username='testuser2', email='test@blub.com')
+        form = SettingsForm(data={
+            'email': 'test@blub.com'}, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], [DUPLICATE_EMAIL_ERROR])
+
+
+class SelectTimeZoneFormTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='test@bla.com',
+            password='test1234')
+
+    def test_clean_timezone_empty(self):
+        form = SelectTimeZoneForm(data={}, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['timezone'], [EMPTY_TIMEZONE_ERROR])
+
+    def test_clean_timezone_invalid(self):
+        form = SelectTimeZoneForm(
+            data={'timezone': 'INVALIDBYINTENT'}, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['timezone'], [INVALID_TIMEZONE_ERROR])
+
+    def test_clean_timezone_valid(self):
+        form = SelectTimeZoneForm(
+            data={'timezone': 'Europe/Berlin'}, instance=self.user)
+        self.assertTrue(form.is_valid())
+
+
+class SubmitCaffeineFormTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='test@bla.com',
+            password='test1234')
+        self.user.timezone = 'Europe/Berlin'
+
+    def test_clean_no_previous(self):
+        form = SubmitCaffeineForm(
+            self.user, DRINK_TYPES.coffee,
+            data={'date': timezone.now()})
+        self.assertTrue(form.is_valid(), str(form.errors))
+
+    def save(self):
+        caffeine = Caffeine(user=self.user, ctype=self.ctype,
+                            date=self.cleaned_data['date'],
+                            timezone=self.user.timezone)
+        caffeine.save()
+        return caffeine
+
+    def test_save_with_passed_data(self):
+        caffeine = SubmitCaffeineForm(
+            self.user, DRINK_TYPES.coffee, data={'date': timezone.now()}
+        ).save()
+        self.assertLessEqual(caffeine.date, timezone.now())
+        self.assertEqual(caffeine.ctype, DRINK_TYPES.coffee)
+        self.assertEqual(caffeine.user, self.user)
+        self.assertEqual(caffeine.timezone, 'Europe/Berlin')
+
+    def test_clean_old_previous(self):
+        Caffeine.objects.create(
+            user=self.user, ctype=DRINK_TYPES.coffee,
+            date=timezone.now() - timedelta(minutes=10))
+        form = SubmitCaffeineForm(
+            self.user, DRINK_TYPES.coffee,
+            data={'date': timezone.now()})
+        self.assertTrue(form.is_valid(), str(form.errors))
+
+    def test_clean_recent_previous(self):
+        Caffeine.objects.create(
+            user=self.user, ctype=DRINK_TYPES.coffee,
+            date=timezone.now() - timedelta(
+                minutes=settings.MINIMUM_DRINK_DISTANCE - 1))
+        form = SubmitCaffeineForm(
+            self.user, DRINK_TYPES.coffee,
+            data={'date': timezone.now()})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors['date']), 1)
+        self.assertRegexpMatches(
+            form.errors['date'][0],
+            r'^Your last %(drink)s was less than %(minutes)d minutes ago' % {
+                'drink': DRINK_TYPES[DRINK_TYPES.coffee],
+                'minutes': settings.MINIMUM_DRINK_DISTANCE
+            })

64f88b66643de5facb0e39486580d8a1de1a381d
Jan Dittberner <jan@dittberner.info>
2014-05-01 14:07:28 +0200

    remove django-discover-runner

--- coffeestats/coffeestats/settings/test.py
+++ coffeestats/coffeestats/settings/test.py
@@ -1,9 +1,3 @@
 from __future__ import absolute_import
 
 from .base import *
-
-########## TEST SETTINGS
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
-TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
-TEST_DISCOVER_ROOT = SITE_ROOT
-TEST_DISCOVER_PATTERN = "test_*.py"

6ec8e01d8e144b498329d4c0fb928bb63943b6cb
Jan Dittberner <jan@dittberner.info>
2014-05-04 16:37:23 +0200

    improve branch coverage of caffeine.admin

--- coffeestats/caffeine/tests/test_admin.py
+++ coffeestats/caffeine/tests/test_admin.py
@@ -12,36 +12,36 @@ User = get_user_model()
 
 class UserCreationFormTest(TestCase):
 
+    def setUp(self):
+        self.testdata = {
+            'username': 'testuser',
+            'email': 'test@bla.com',
+            'password1': 'test1234',
+            'password2': 'test1234'
+        }
+
     def test_clean_password2_passwords_match(self):
-        form = UserCreationForm(
-            data={'username': 'testuser',
-                  'email': 'test@bla.com',
-                  'password1': 'test1234',
-                  'password2': 'test1234'}
-        )
+        form = UserCreationForm(data=self.testdata)
         self.assertTrue(form.is_valid(), str(form.errors))
         self.assertEqual(form.cleaned_data['password2'], 'test1234')
 
     def test_clean_password2_passwords_do_not_match(self):
-        form = UserCreationForm(
-            data={'username': 'testuser',
-                  'email': 'test@bla.com',
-                  'password1': 'test1234',
-                  'password2': 'test2345'}
-        )
+        self.testdata['password2'] = 'test2345'
+        form = UserCreationForm(data=self.testdata)
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors['password2']), 1)
         self.assertEqual(form.errors['password2'], [PASSWORD_MISMATCH_ERROR])
 
     def test_save_hash_password(self):
-        form = UserCreationForm(
-            data={'username': 'testuser',
-                  'email': 'test@bla.com',
-                  'password1': 'test1234',
-                  'password2': 'test1234'}
-        )
+        form = UserCreationForm(data=self.testdata)
         user = form.save()
+        self.assertIsNotNone(user.pk)
         self.assertTrue(user.check_password('test1234'))
+
+    def test_no_commit(self):
+        form = UserCreationForm(data=self.testdata)
+        user = form.save(commit=False)
+        self.assertIsNone(user.pk)
 
 
 class UserChangeFormTest(TestCase):

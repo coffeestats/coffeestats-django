49f799ceaee733d0cd1eabe4c3e05d06e641d0cc
Jan Dittberner <jan@dittberner.info>
2014-05-11 00:28:27 +0200

    working production settings

--- coffeestats/coffeestats/settings/base.py
+++ coffeestats/coffeestats/settings/base.py
@@ -52,7 +52,7 @@ TEMPLATE_DEBUG = DEBUG
 ########## MANAGER CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
-    ('Your Name', 'your_email@example.com'),
+    ('Coffeestats Team', get_env_variable('COFFEESTATS_SITE_ADMINMAIL')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers

34c43020496ef61b03d79d2fbdb592676266bc54
Jeremias Arnstadt <j.arnstadt@art-ifact.de>
2014-06-05 14:06:59 +0200

    - added new icon to font - added more responsiveness - added touch dropdown handling

--- coffeestats/static/js/scripts.js
+++ coffeestats/static/js/scripts.js
@@ -2,10 +2,30 @@ $(document).ready(function(){
 	var coffeestats = {
 		init: function () {
 	      coffeestats.equalHeightsBoxes();
+	      coffeestats.showNavigation();
+	      coffeestats.showMenuDropdown();
+	      coffeestats.showLoginDropdown();
 	    },
 
 		equalHeightsBoxes : function() {
 			$('.white-box').not('.fullWidth').equalHeights();
+		},
+
+		showNavigation : function() {
+			$('.menuIndicator').click(function(){
+				$(this).toggleClass('hover');
+			});
+		},
+
+		showMenuDropdown : function() {
+			$('.mainNav li span.settings').click(function(){
+				$(this).parent('li').toggleClass('hover');
+			});
+		},
+		showLoginDropdown : function() {
+			$('.login li span').click(function(){
+				$(this).parent('li').toggleClass('hover');
+			});
 		}
 	}
 	coffeestats.init();

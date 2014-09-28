a83af4989000632ca20287d05172ba9caa23606b
Jan Dittberner <jan@dittberner.info>
2014-09-28 21:40:17 +0200

    switch to Django 1.7 (resolves #38)
    * add initial migration for caffeine app
    * remove South from settings
    * add TEST_RUNNER to settings to fix test runner compatibility warning
    * bump Django dependency to 1.7
    * drop South dependency
    * replace abandoned django-registration with django-registration-redux

--- coffeestats/coffeestats/settings/base.py
+++ coffeestats/coffeestats/settings/base.py
@@ -313,13 +313,4 @@ LOGGING = {
 WSGI_APPLICATION = '%s.wsgi.application' % SITE_NAME
 ########## END WSGI CONFIGURATION
 
-
-########## SOUTH CONFIGURATION
-# See: http://south.readthedocs.org/en/latest/installation.html#configuring-your-django-installation
-INSTALLED_APPS += (
-    # Database migration helpers:
-    'south',
-)
-# Don't need to use South when setting up a test database.
-SOUTH_TESTS_MIGRATE = False
-########## END SOUTH CONFIGURATION
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'

8187721f256af872b76b68870c8a1bc053967c6d
Jan Dittberner <jan@dittberner.info>
2015-01-29 16:46:40 +0100

    remove unused import

--- coffeestats/caffeine/views.py
+++ coffeestats/caffeine/views.py
@@ -4,7 +4,6 @@ from django.core.urlresolvers import (
     reverse_lazy,
 )
 from django.http import (
-    HttpResponseBadRequest,
     HttpResponseRedirect,
 )
 from django.template.loader import render_to_string

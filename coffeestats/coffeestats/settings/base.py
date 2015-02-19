29a9a1e4778d29bad54f8ecf937e1fa5fcda3f1d
Jan Dittberner <jan@dittberner.info>
2015-02-20 00:27:45 +0100

    add SessionAuthentication to DEFAULT_AUTHENTICATION_CLASSES

--- coffeestats/coffeestats/settings/base.py
+++ coffeestats/coffeestats/settings/base.py
@@ -256,6 +256,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',

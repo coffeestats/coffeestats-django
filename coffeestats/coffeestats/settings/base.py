164c2a8fb60ff027e829815a08d11d60ee1946ba
Jan Dittberner <jan@dittberner.info>
2016-09-17 18:04:09 +0200

    Allow all authenticated users to use the API

--- coffeestats/coffeestats/settings/base.py
+++ coffeestats/coffeestats/settings/base.py
@@ -259,7 +259,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'PAGINATE_BY': 10
 }

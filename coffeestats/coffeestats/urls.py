636e0bd964892e97a58cfd4f7fcd43537f5bc8bf
Jan Dittberner <jan@dittberner.info>
2016-09-17 19:43:42 +0200

    Move API URL setup into caffeine_api_v2.urls module

--- coffeestats/coffeestats/urls.py
+++ coffeestats/coffeestats/urls.py
@@ -5,30 +5,14 @@ from django.views.i18n import javascript_catalog
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
-from rest_framework_nested import routers
-
-from caffeine_api_v2 import views as apiv2_views
-
 
 admin.autodiscover()
-
-# Routers for determining the URL conf
-router = routers.SimpleRouter()
-router.register(r'users', apiv2_views.UserViewSet)
-router.register(r'caffeine', apiv2_views.CaffeineViewSet)
-
-users_router = routers.NestedSimpleRouter(router, r'users', lookup='caffeine')
-users_router.register(
-    r'caffeine', apiv2_views.UserCaffeineViewSet, base_name='user-caffeine')
 
 urlpatterns = [
     url(r'^', include('caffeine.urls')),
     url(r'^api/v1/', include('caffeine_api_v1.urls', 'apiv1')),
     # new API
-    url(r'^api/v2/', include(router.urls)),
-    url(r'^api/v2/', include(users_router.urls)),
-    url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/v2/', include('caffeine_api_v2.urls')),
     # oauth2
     url(r'^oauth2/',
         include('oauth2_provider.urls', namespace='oauth2_provider')),

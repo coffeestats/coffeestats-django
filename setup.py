d98c66ee6a0e27485980fd336ef2ee7a2f08462c
Jan Dittberner <jan@dittberner.info>
2018-10-06 23:09:01 +0200

    Add setup.py for Read the Docs

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='coffeestats',
+      version='0.2.1',
+      description='Coffeestats.org',
+      author='Jan Dittberner',
+      author_email='jan@dittberner.info',
+      url='https://github.com/coffeestats/coffeestats-django',
+      package_dir = {'': 'coffeestats'},
+)

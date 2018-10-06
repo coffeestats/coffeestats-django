bdedb3b03170494da8b5527a0e156b475e6ba454
Jan Dittberner <jan@dittberner.info>
2018-10-06 21:32:54 +0200

    Initialize Django code for docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,15 @@
 import sys
 import os
 
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'coffeestats'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffeestats.settings.local')
+
+from django import setup as django_setup
+django_setup()
 
 # -- General configuration -----------------------------------------------------
 

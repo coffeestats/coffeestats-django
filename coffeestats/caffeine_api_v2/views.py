ce7581ba0359db6312b906472e365c67fe2da562
Jan Dittberner <jan@dittberner.info>
2024-05-13 16:46:30 +0200

    Fix API bug caused by dot in username
    Fix generation of user URLs in the API by adding a lookup_value_regex
    for username.

--- coffeestats/caffeine_api_v2/views.py
+++ coffeestats/caffeine_api_v2/views.py
@@ -15,7 +15,8 @@ class CaffeineViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows caffeine entries to be viewed.
     """
-    queryset = Caffeine.objects.all().order_by('-date')
+
+    queryset = Caffeine.objects.all().order_by("-date")
     serializer_class = CaffeineSerializer
 
 
@@ -23,31 +24,38 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows users to be viewed.
     """
-    queryset = User.objects.all().order_by('username')
+
+    queryset = User.objects.all().order_by("username")
     serializer_class = UserSerializer
-    lookup_field = 'username'
+    lookup_field = "username"
+    lookup_value_regex = r"[\w0-9@.+-_]+"
 
 
 class UserCaffeineViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows working with a users caffeine entries.
     """
+
     serializer_class = UserCaffeineSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnCaffeineOrReadOnly,
-                          IsOwnerOrReadOnly,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnCaffeineOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
     _view_owner = None
 
     def _get_view_owner(self):
         if self._view_owner is None:
             self._view_owner = User.objects.get(
-                username=self.kwargs['caffeine_username'])
+                username=self.kwargs["caffeine_username"]
+            )
         return self._view_owner
+
     view_owner = property(_get_view_owner)
 
     def get_queryset(self):
-        return self.view_owner.caffeines.all().order_by('-date')
+        return self.view_owner.caffeines.all().order_by("-date")
 
 
 class UsageAgreement(LoginRequiredMixin, TemplateView):
-    template_name = 'caffeine_api_v2/api_usage_agreement.html'
+    template_name = "caffeine_api_v2/api_usage_agreement.html"

c31b8a554f165645bea4ececcb24e1e9220de52e
Jan Dittberner <jan@dittberner.info>
2026-08-07 13:24:31 +0200

    Update to Django 5.2
    - update dependencies
    - remove test broken by the introduction of django-environ

--- coffeestats/coffeestats/tests.py
+++ coffeestats/coffeestats/tests.py
@@ -1,27 +1,4 @@
-import os
 from unittest import TestCase
-
-from django.core.exceptions import ImproperlyConfigured
-
-from coffeestats.settings.base import get_env_variable
-
-
-class GetEnvVariableTest(TestCase):
-
-    def test_get_existing_env_variable(self):
-        os.environ['testvariable'] = 'myvalue'
-        self.assertEqual(get_env_variable('testvariable'), 'myvalue')
-
-    def test_get_missing_env_variable(self):
-        if 'missingvariable' in os.environ:
-            del os.environ['missingvariable']
-        try:
-            get_env_variable('missingvariable')
-            self.fail('should have raised ImproperlyConfigured')
-        except Exception as e:
-            self.assertIsInstance(e, ImproperlyConfigured)
-            self.assertEqual(
-                str(e), 'Set the missingvariable environment variable')
 
 
 class WSGITest(TestCase):
